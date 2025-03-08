062bf76938494028abc47eed21b4c4a793d8d50e
abhishek.sat <abhisheksatre@gmail.com>
2025-03-08 20:09:58 +0530

    port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/', homeRoute);
 app.use('/contact', contactRoute);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
